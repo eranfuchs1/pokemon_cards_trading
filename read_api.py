59882bf77eb2fa86c28406de830b6c1403e4342c
xer <xer@xer-labtop>
2021-08-30 10:06:27 +0300

    read_api.py, reading the api

--- read_api.py
+++ read_api.py
@@ -3,3 +3,10 @@ import requests
 
 def get_json(url):
     return requests.get(url).json()
+
+
+def iterate_with_id(url, id_beg=0):
+    json_obj = get_json(f'{url}{id_beg}')
+    while json_obj:
+        id_beg += 1
+        json_obj = get_json(f'{url}{id_beg}')
