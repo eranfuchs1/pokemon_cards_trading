7ad0324edc1fe86f1c4fcedf3da13708f9039a60
xer <xer@xer-labtop>
2021-09-03 14:59:53 +0300

    signal to initialize new users with user_owned_cards instance

---
+++ trade/signals.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from .models import UserOwnedCards
+
+
+def init_user_owned_cards(sender, instance, **kwargs):
+    user_owned_cards = UserOwnedCards.objects.create(user=instance)
+    user_owned_cards.save()
+
+
+post_save.connect(init_user_owned_cards, sender=User)
