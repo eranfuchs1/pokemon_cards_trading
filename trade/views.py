ad45b9c4a9deb34c7878f70fb9e3360150ab6a3f
xer <xer@xer-labtop>
2021-09-03 14:28:04 +0300

    autopep8 for the trade/views.py file

--- trade/views.py
+++ trade/views.py
@@ -1,32 +1,36 @@
+from .models import Card, TradeOffer, TradePosting, UserOwnedCards
+from .trade_offer import accept_and_remove, accept_trade_offer, post_trade, post_trade_offer, remove_trade_posting
+from .forms import OfferTradeModelForm, PostTradeModelForm, TradeOfferAcceptModelForm
 from django.shortcuts import render, redirect
 
 # Create your views here.
 nav_links = ['homepage', ]
 
+
 def homepage_view(request):
     context = {}
     context['nav_links'] = nav_links
     context['title'] = 'Trade Homepage'
-    return render(request,'homepage.html', context=context)
+    return render(request, 'homepage.html', context=context)
 
 
-
-from .forms import OfferTradeModelForm, PostTradeModelForm, TradeOfferAcceptModelForm
-from .trade_offer import accept_and_remove, accept_trade_offer, post_trade, post_trade_offer, remove_trade_posting
-from .models import Card, TradeOffer, TradePosting, UserOwnedCards
 def card_collection_view(request):
     context = {}
     try:
-        context['user_owned_cards'] = UserOwnedCards.objects.get(user=request.user.id)
+        context['user_owned_cards'] = UserOwnedCards.objects.get(
+            user=request.user.id)
     except UserOwnedCards.DoesNotExist:
         pass
     context['nav_links'] = nav_links
     context['title'] = 'Your Cards Collection'
     return render(request, 'card_collection.html', context=context)
+
+
 def card_trade_posting_view(request):
     context = {}
     try:
-        context['user_owned_cards'] = UserOwnedCards.objects.get(user=request.user.id)
+        context['user_owned_cards'] = UserOwnedCards.objects.get(
+            user=request.user.id)
     except UserOwnedCards.DoesNotExist:
         pass
     context['nav_links'] = nav_links
