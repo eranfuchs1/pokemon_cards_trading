e3f290a8156cda9aa6714da20ce83266a80d427f
xer <xer@xer-labtop>
2021-09-03 14:16:23 +0300

    routing the card_collection_view in the urlpatterns

--- trade/urls.py
+++ trade/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import homepage_view
+from .views import homepage_view, card_collection_view
 
 urlpatterns = [
     #path('login/', LoginView.as_view(), name='login'),
-    path('homepage/', homepage_view, name='homepage')
+    path('homepage/', homepage_view, name='homepage'),
+    path('card_collection/', card_collection_view, name='card_collection'),
 ]
