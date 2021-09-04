4a8fba87447301239a2e0911f9e4be0647e3ae94
xer <xer@xer-labtop>
2021-09-04 21:23:44 +0300

    random_card_collection taking optional argument for total number of cards given, defaults to 10

--- trade/random_card_collection.py
+++ trade/random_card_collection.py
@@ -2,8 +2,8 @@ import random
 from .models import Card
 
 
-def random_card_collection():
+def random_card_collection(num_of_cards=10):
     all_cards = list(Card.objects.all())
     random.shuffle(all_cards)
-    random_cards = all_cards[:10]
+    random_cards = all_cards[:num_of_cards]
     return random_cards
