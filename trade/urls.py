21d6d337ac0872c863deeb4ac33cae92a12c4e05
xer <xer@xer-labtop>
2021-09-04 22:36:41 +0300

    trade offer view, trade offers view for viewing all trade offers for the user

--- trade/urls.py
+++ trade/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homepage_view, card_collection_view, card_trade_posting_view, trade_posts_view
+from .views import homepage_view, card_collection_view, card_trade_posting_view, trade_posts_view, trade_offer_view, trade_offers_view
 
 urlpatterns = [
     #path('login/', LoginView.as_view(), name='login'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('card_collection/', card_collection_view, name='card_collection'),
     path('trade_posting/', card_trade_posting_view, name='trade_posting'),
     path('trade_posts/', trade_posts_view, name='trade_posts'),
+    path('trade_offer/<int:trade_post_id>/', trade_offer_view, name='trade_offer'),
+    path('trade_offers/', trade_offers_view, name='trade_offers'),
 ]
--- trade/views.py
+++ trade/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-nav_links = ['homepage', 'card_collection', 'trade_posting', 'trade_posts', ]
+nav_links = ['homepage', 'card_collection', 'trade_posting', 'trade_posts', 'trade_offers', ]
 
 
 def homepage_view(request):
@@ -57,3 +57,37 @@ def trade_posts_view(request):
     context['title'] = 'Trade Posts'
     context['trade_posts'] = TradePosting.objects.all()
     return render(request, 'trade_posts.html', context=context)
+
+
+def trade_offer_view(request, trade_post_id):
+    context = {}
+    context['nav_links'] = nav_links
+    context['title'] = 'Post Trade Offer'
+    try:
+        context['user_owned_cards'] = UserOwnedCards.objects.get(
+            user=request.user.id)
+    except UserOwnedCards.DoesNotExist:
+        pass
+    context['posted_trade_cards'] = TradePosting.objects.get(id=trade_post_id)
+    context['form'] = OfferTradeModelForm()
+    if request.method == 'GET':
+        return render(request, 'trade_offer.html', context=context)
+    elif request.method == 'POST':
+        form = OfferTradeModelForm(request.POST)
+        if form.is_valid():
+            trade_offer = form.save(commit=False)
+            trade_offer.user = request.user
+            trade_offer.save()
+            form.save()
+            context['posted_trade_cards'].trade_offers.add(trade_offer)
+            context['posted_trade_cards'].save()
+
+        return redirect('homepage')
+
+@login_required
+def trade_offers_view(request):
+    context = {}
+    context['nav_links'] = nav_links
+    context['title'] = 'Trade Offers'
+    context['trade_posts'] = TradePosting.objects.filter(trade_poster=request.user)
+    return render(request, 'trade_offers.html', context=context)
