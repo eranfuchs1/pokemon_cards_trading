d03b876e1b584b5bf4ce072f3f2a05b09efc89e6
Eran Fuchs <electranoob@outlook.com>
2023-11-17 17:18:08 +0200

    redirect to homepage on empty url path parameters

--- cards/urls.py
+++ cards/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='trade/homepage/'), name='homepage'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('trade/', include('trade.urls')),
--- trade/urls.py
+++ trade/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import homepage_view, card_collection_view, card_trade_posting_view, trade_posts_view, trade_offer_view, trade_offers_view
 
 urlpatterns = [
-    #path('login/', LoginView.as_view(), name='login'),
     path('homepage/', homepage_view, name='homepage'),
     path('card_collection/', card_collection_view, name='card_collection'),
     path('trade_posting/', card_trade_posting_view, name='trade_posting'),
